4e9be5186713b3e891c672928850dfaccb03cbd6
Dean Kopping <dkopping@plusonex.com>
2025-02-15 19:18:30 +0200

    fonts loading

--- src/Facilities.tsx
+++ src/Facilities.tsx
@@ -1,3 +1,4 @@
+import { useMediaQuery } from "react-responsive";
 import "./styles/Facilities.css";
 
 import React from "react";
@@ -15,6 +16,7 @@ const images = Object.values(imageFiles);
 export type ItemProps = React.ButtonHTMLAttributes<HTMLDivElement>;
 
 export default function Facilities() {
+  const isMobile = useMediaQuery({ query: "(max-width: 700px)" });
   function Items({ children, ...props }: ItemProps) {
     return (
       <div className="items" {...props}>
@@ -33,7 +35,11 @@ export default function Facilities() {
               <li className="artwork-item">
                 <picture>
                   <source srcSet={`${artwork} 400w`}></source>
-                  <img src={artwork} height={400} width={400}></img>
+                  <img
+                    src={artwork}
+                    height={isMobile ? 280 : 400}
+                    width={isMobile ? 280 : 400}
+                  ></img>
                 </picture>
               </li>
             </div>
