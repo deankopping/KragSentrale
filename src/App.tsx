f3e185fae6557b63fd7eb703586b0bdc924d1705
jacpa42 <jacobenthoven@gmail.com>
2025-06-08 19:04:03 +0200

    Added voltage path

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import MasonryGrid from "./MasonaryGrid";
 import WaiverComplete from "./WaiverComplete";
+import Voltage from "./Voltage";
 import EmailVerified from "./EmailVerified";
 import Redirect from "./Redirect";
 
@@ -11,8 +12,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/gallery" element={<MasonryGrid />} />
+        <Route path="/voltage" element={<Voltage />} />
 				<Route path="/waiver" element={<Redirect to="https://waiver.smartwaiver.com/w/namxoiwk5vpgif6j9adqik/web/" />} />
-				<Route path="/voltage" element={<Redirect to="https://docs.google.com/forms/d/e/1FAIpQLSd7N8U1y-MaZQL97rEluGZsdzSLo53NZ2R-nfL_Iyse95DscA/viewform?usp=dialog" />} />
 				<Route path="/waiver/complete" element={<WaiverComplete />} />
 				<Route path="/waiver/email-verified" element={<EmailVerified />} />
       </Routes>
